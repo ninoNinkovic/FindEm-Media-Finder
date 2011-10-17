409fa3a81d1cb13262947c6fd4a6d331eced58cd
Caleb Call <calebcall@me.com>
2011-10-16 21:09:16 -0600

    Initial Add of FindEm script

---
+++ findem.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use File::Find::Rule;
+use Data::Dumper;
+
+my $handbrake = '/usr/local/bin/HandBrakeCLI';
+my $preset = 'AppleTV';
+my $itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes/';
+my $tvtag = '/Users/caleb/Documents/bin/tvtag-sickbeard.pl';
+
+my @files = find(
+	file =>
+	name => [qw/ *.mkv *.avi / ],
+	in => @ARGV
+	);
+
+
+$list = join("\n",@files," ");
+print "\nFound the following files to convert:\n\n$list\n";
+
+
+
+foreach $file (@files) {
+	print $file . "\n";
+	system "$handbrake -i '$file' -o test\.m4v --preset=$preset";
+	#system "$tvtag test.m4v"
+}
