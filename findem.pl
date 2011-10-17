56238759e9127d8a7b46c6430fa51e11073b175a
Caleb Call <calebcall@me.com>
2011-10-16 21:59:08 -0600

    Updating so it can deal with spaces and replaces avi/mkv with m4v for the output file.  Also adding tagging, moving original file to archive directory, and moving file in to iTunes directory so it auto adds to iTunes.

--- findem.pl
+++ findem.pl
@@ -1,12 +1,16 @@
 #!/usr/bin/env perl
 
+use strict;
+use warnings;
 use File::Find::Rule;
+use File::Copy;
 use Data::Dumper;
 
 my $handbrake = '/usr/local/bin/HandBrakeCLI';
 my $preset = 'AppleTV';
-my $itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes/';
-my $tvtag = '/usr/local/bin/tvtag-sickbeard.pl';
+my $itunes = '"/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes/"';
+my $tvtag = '/Users/caleb/Documents/bin/tvtag-sickbeard.pl';
+my $archive = '/Users/caleb/Movies/archive';
 
 my @files = find(
 	file =>
@@ -15,13 +19,21 @@ my @files = find(
 	);
 
 
-$list = join("\n",@files," ");
+my $list = join("\n",@files," ");
 print "\nFound the following files to convert:\n\n$list\n";
 
 
-
-foreach $file (@files) {
-	print $file . "\n";
-	system "$handbrake -i '$file' -o test\.m4v --preset=$preset";
-	#system "$tvtag test.m4v"
+foreach my $infile (@files) {
+    print $infile . "\n";
+    # escape spaces
+    # $infile =~ s[ ][\\ ]g;
+    my $outfile = $infile;
+    $outfile =~ s/\.(?:mkv|avi)\z/\.m4v/;
+    system "$handbrake -i '$infile' -o '$outfile' --preset=$preset"; # without escaped spaces
+    #system "$handbrake -i $infile' -o $outfile --preset=$preset"; # with escaped spaces
+	system "$tvtag $outfile";
+	move ($infile, $archive); 
+	sleep (2);
+	copy ($outfile, $itunes);
+	sleep (2);
 }
