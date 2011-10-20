6048631ed63392ed8d5d1c1196d319598a46bc46
Caleb Call <calebcall@me.com>
2011-10-19 22:33:07 -0600

    Updating changes, this is currently not working.  Working on adding ability to handle movies and tv shows.

--- findem.pl
+++ findem.pl
@@ -1,22 +1,26 @@
 #!/usr/bin/env perl
 
-use strict;
+#use strict;
 use warnings;
 use File::Find::Rule;
 use File::Copy;
+use MP4::Info;
 use Data::Dumper;
 
 my $handbrake = '/usr/local/bin/HandBrakeCLI';
 my $preset = 'AppleTV';
-my $itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes/';
+my $itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes';
 #my $tvtag = '$HOME/Documents/bin/tvtag-sickbeard.pl';
-my $tvtag = '$HOME/svn/FindEm-Movie-Finder/tvtag-sickbeard.pl';
-my $archive = '/Users/caleb/Movies/archive/';
+my $tvtag = '/Users/caleb/svn/FindEm-Movie-Finder/tvtag-sickbeard.pl';
+my $archive = '/Users/caleb/Movies/archive';
+my $mp4info = '/usr/local/bin/mp4info';
+my $ripped = '/Users/caleb/Movies/Ripped';
+#my $count = system "'$mp4info' '$outfile' | grep -c 'TV Season: 0'";
 
 my @files = find(
 	file =>
 	name => [qw/ *.mkv *.avi / ],
-	in => @ARGV
+	in => \@ARGV
 	);
 
 
@@ -32,12 +36,36 @@ foreach my $infile (@files) {
     $outfile =~ s/\.(?:mkv|avi)\z/\.m4v/;
     system "$handbrake -i '$infile' -o '$outfile' --preset=$preset"; # without escaped spaces
     #system "$handbrake -i $infile' -o $outfile --preset=$preset"; # with escaped spaces
-	system ("mv '$infile' '$archive'");
 	#move ('$infile', '$archive');
+	print "Executing '$tvtag' '$outfile' \n";
+	sleep (5);
+	system "'$tvtag' '$outfile'";
 	sleep (2);
-	system "$tvtag $outfile";
-	sleep (2);
-	system ("cp '$outfile' '$itunes'");
-	#copy ($outfile, $itunes);
-	sleep (2);
+	#system ("mv '$infile' '$archive'");
+	print $infile . "\n";
+	print $outfile . "\n";
+	print $archive . "\n";
+	#print $count . "\n";
+	
+	my $info = get_mp4info($outfile) or die "No TAG info";
+	$info->{TITLE};
+	if (-e $info) {
+		copy ($outfile,$itunes) or die "Copy failed: $!";
+		sleep (2);
+	} else {
+		move ($outfile,$ripped) or die "Move failed: $!";
+		sleep (5);
+	}
+	
+	#print Dumper(get_mp4info($outfile));
+	move ($infile,$archive) or die "Move failed: $!";
+ ####   sleep (5);
+ ####   if ($count == 0) {
+ ####   	move ($outfile,$ripped) or die "Move failed: $!";
+ ####   	sleep (5);
+ ####   } else {
+ ####   #system ("cp '$outfile' '$itunes'");
+ ####   copy ($outfile,$itunes) or die "Copy failed: $!";
+ ####   sleep (2);
+ ####   }
 }
