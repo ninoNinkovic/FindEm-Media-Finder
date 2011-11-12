9594b875755665bef238f3418e6dffc1cb94c955
Caleb Call <calebcall@me.com>
2011-11-11 20:52:55 -0700

    Working version
    Working version, using a config file which builds on first run.  Will
    handle multiple directories, if the file is a movie file named
    appropriately (<movie name (year).ext>) it will move it to a separate
    "ripped" directory so they can be tagged manually.  TV files will tag
    automatically out of your local Sickbeard database.

--- findem.pl
+++ findem.pl
@@ -1,32 +1,47 @@
 #!/usr/bin/env perl
 
-#use strict;
-use warnings;
 use File::Find::Rule;
 use File::Copy;
-use MP4::Info;
+use File::Path;
+#use MP4::Info;
 use Data::Dumper;
+use File::Basename;
+use Cwd;
 
-my $handbrake = '/usr/local/bin/HandBrakeCLI';
-my $preset = 'AppleTV';
-my $itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes';
-#my $tvtag = '$HOME/Documents/bin/tvtag-sickbeard.pl';
-my $tvtag = '/Users/caleb/svn/FindEm-Movie-Finder/tvtag-sickbeard.pl';
-my $archive = '/Users/caleb/Movies/archive';
-my $mp4info = '/usr/local/bin/mp4info';
-my $ripped = '/Users/caleb/Movies/Ripped';
-#my $count = system "'$mp4info' '$outfile' | grep -c 'TV Season: 0'";
+my $config = "$ENV{HOME}/.findem/config";
 
+#######################################################################################
+# READ IN CONFIG FILE FROM ~/.findem/config
+#######################################################################################
+if ( -e "$ENV{HOME}/.findem") {
+	if ( -e "$config") {
+		do "$ENV{HOME}/.findem/config";
+	} else {
+		define_config();
+	}
+} else {
+	define_config();
+}
+
+#######################################################################################
+
+#######################################################################################
+# Find files to Convert
+#######################################################################################
 my @files = find(
 	file =>
 	name => [qw/ *.mkv *.avi / ],
 	in => \@ARGV
 	);
 
-
 my $list = join("\n",@files," ");
 print "\nFound the following files to convert:\n\n$list\n";
 
+#######################################################################################
+
+#######################################################################################
+# Loop through files found and convert to .m4v
+#######################################################################################
 
 foreach my $infile (@files) {
     print $infile . "\n";
@@ -34,38 +49,119 @@ foreach my $infile (@files) {
     # $infile =~ s[ ][\\ ]g;
     my $outfile = $infile;
     $outfile =~ s/\.(?:mkv|avi)\z/\.m4v/;
+	my $base_in = basename "$infile";
+	my $base_out = basename "$outfile";
+	sleep (2);
     system "$handbrake -i '$infile' -o '$outfile' --preset=$preset"; # without escaped spaces
     #system "$handbrake -i $infile' -o $outfile --preset=$preset"; # with escaped spaces
-	#move ('$infile', '$archive');
+
+#######################################################################################
+
+#######################################################################################
+# Determine if it's a Movie or TV Show and handle appropriately
+#######################################################################################
+
+	if ($outfile =~ m/\(\d{4}\).m4v\z/){
+		print "Moving and Copying files around... \n\n";
+		sleep (2);
+		move ($outfile,$ripped) or die "Move of Movie file failed: $!";
+		move ($infile,$archive) or die "Move of Original Movie file failed: $!";
+	}else{
 	print "Executing '$tvtag' '$outfile' \n";
-	sleep (5);
+	sleep (2);
 	system "'$tvtag' '$outfile'";
 	sleep (2);
-	#system ("mv '$infile' '$archive'");
-	print $infile . "\n";
-	print $outfile . "\n";
-	print $archive . "\n";
-	#print $count . "\n";
 	
-	my $info = get_mp4info($outfile) or die "No TAG info";
-	$info->{TITLE};
-	if (-e $info) {
-		copy ($outfile,$itunes) or die "Copy failed: $!";
-		sleep (2);
-	} else {
-		move ($outfile,$ripped) or die "Move failed: $!";
-		sleep (5);
+	print "Moving and Copying files around... \n\n";
+	copy ($outfile,$itunes_tmp) or die "Move Failed: $!";
+	move ($infile,$archive) or die "Copy failed: $!";
+	sleep (5);
+	move ("$itunes_tmp/$base_out",$itunes) or die "Move failed: $!";
 	}
 	
-	#print Dumper(get_mp4info($outfile));
-	move ($infile,$archive) or die "Move failed: $!";
- ####   sleep (5);
- ####   if ($count == 0) {
- ####   	move ($outfile,$ripped) or die "Move failed: $!";
- ####   	sleep (5);
- ####   } else {
- ####   #system ("cp '$outfile' '$itunes'");
- ####   copy ($outfile,$itunes) or die "Copy failed: $!";
- ####   sleep (2);
- ####   }
+ 	#print Dumper(get_mp4info($outfile));
 }
+
+#######################################################################################
+
+#######################################################################################
+# Define the Config file
+#######################################################################################
+
+sub define_config ($config) {
+	system("clear");
+	print "You don't appear to have a config file. Let's build one. (Hit enter to accept default)\n\n";
+	sleep(1);
+	if ( !-e "$ENV{HOME}/.findem") {
+		mkpath("$ENV{HOME}/.findem");	
+	}
+	
+	print "Location of HandBrakeCLI? [/usr/local/bin/HandBrakeCLI (default)]: ";
+	chomp ($handbrake = <STDIN>);
+	if ($handbrake eq '') {
+		$handbrake = '/usr/local/bin/HandBrakeCLI';
+	}
+	print "Define HandBrake Preset to use [AppleTV (default)]: ";
+	chomp ($preset = <STDIN>);
+	if ($preset eq '') {
+		$preset = 'AppleTV';
+	}
+
+   ### do {
+   ### 	print "Define Location of your iTunes Auto Add Directory: ";
+   ### 	chomp ($mp4tagger = <STDIN>);
+   ### } until "$mp4tagger" ne "";
+		
+	print "Define Location of your iTunes Auto Add Directory [/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes (default)]: ";
+	chomp ($itunes = <STDIN>);
+	if ($itunes eq '') {
+		$itunes = '/Volumes/Media/iTunes/iTunes Media/Automatically Add to iTunes';
+	}
+	
+	print "Define Location of your iTunes Temp Directory [/Volumes/Media/iTunes/iTunes Media (default)]: ";
+	chomp ($itunes_tmp = <STDIN>);
+	if ($itunes_tmp eq '') {
+		$itunes_tmp = '/Volumes/Media/iTunes/iTunes Media';
+	}
+
+	print "Define TV Tag script: [$ENV{HOME}/svn/FindEm-Movie-Finder/tvtag-sickbeard.pl (default)]: ";
+	chomp ($tvtag = <STDIN>);
+	if ($tvtag eq '') {
+		$tvtag = '$ENV{HOME}/svn/FindEm-Movie-Finder/tvtag-sickbeard.pl';
+	}
+	
+	print "Define archive directory: [$ENV{HOME}/Movies/archive (default)]: ";
+	chomp ($archive = <STDIN>);
+	if ($archive eq '') {
+		$archive = '$ENV{HOME}/Movies/archive';
+	}
+	
+	print "Define Ripped Movies directory: [$ENV{HOME}/Movies/Ripped (default)]: ";
+	chomp ($ripped = <STDIN>);
+	if ($ripped eq '') {
+		$ripped = '$ENV{HOME}/Movies/Ripped';
+	}
+	
+   #####do {
+   #####	print "Define Sickbeard directory: ";
+   #####	chomp ($sickbeard = <STDIN>);
+   #####	$sickbeard =~ s/^~/$ENV{HOME}/g;
+   #####} until "$sickbeard" ne "";	
+	
+	open (FILE, ">>$config");
+	print FILE "\$handbrake = \'$handbrake\'\;\n";
+	print FILE "\$preset = \'$preset\'\;\n";
+ 	print FILE "\$itunes = \'$itunes\'\;\n";
+	print FILE "\$itunes_tmp = \'$itunes_tmp\'\;\n";
+	print FILE "\$tvtag = \"$tvtag\"\;\n";
+	print FILE "\$archive = \"$archive\"\;\n";
+	print FILE "\$ripped = \"$ripped\"\;\n";
+	###print FILE "\$sickbeard = \"$sickbeard\"\;\n";
+	###print FILE "\$sickbearddb = \"$sickbeard/sickbeard.db\"\;\n";
+	
+	print "\n";
+	print "Your config file should be built now. Please run this script again.\n\n";
+	exit;
+}
+
+#######################################################################################
